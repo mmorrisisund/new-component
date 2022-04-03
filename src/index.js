3b543c7407ea424ab5b458c2dbc837798da31670
Mike Morris <michael.a.morris.public@gmail.com>
2022-04-02 21:46:17 -0500

    add main functionality

--- src/index.js
+++ src/index.js
@@ -3,35 +3,68 @@
 const fs = require('fs')
 const fsp = require('fs/promises')
 const path = require('path')
-const { program } = require('commander')
+const { program, Option } = require('commander')
+const { getConfig, getPrettier, getExtensions } = require('./helpers')
 
 async function main() {
   // Load our package.json, so that we can pass the version onto `commander`.
+  const { version } = require('../package.json')
 
   // Get the default config for this component (looks for local/global overrides,
   // falls back to sensible defaults).
+  const config = getConfig()
 
   // Convenience wrapper around Prettier, so that config doesn't have to be
   // passed every time.
+  const prettify = getPrettier(config.prettierConfig)
 
   // Build CLI here
-  program.argument('<componentName>').parse(process.argv)
+  program
+    .version(version)
+    .argument('<componentName>')
+    .addOption(
+      new Option(
+        '-t, --type <componentType>',
+        'Type of React component to generate (default: functional)'
+      )
+        .choices(['functional', 'class', 'pure'])
+        .default(config.type)
+    )
+    .addOption(
+      new Option(
+        '-x, --extension <fileExtension>',
+        'Which file extension to use for the component (default: js)'
+      )
+        .choices(['javascript', 'typescript'])
+        .default(config.extension)
+    )
+    .option(
+      '-d, --dir <pathToDirectory>',
+      'Path to the "components" directory (default: src/components)',
+      config.dir
+    )
+    .parse(process.argv)
 
   // get component name
   const [componentName] = program.args
+
+  // get extensions
+  const [componentExt, indexExt] = getExtensions(program.opts().type)
+
   // Find the path to the selected template file.
-  const templatePath = `./templates/functional.js`
+  const templatePath = `./templates/${program.opts().type}.js`
 
   // Get all of our file paths worked out, for the user's project.
-  const componentDir = `src/components/${componentName}`
-  const componentPath = `${componentDir}/${componentName}.js`
-  const indexPath = `${componentDir}/index.js`
+  const componentDir = `${program.opts().dir}/${componentName}`
+  const componentPath = `${componentDir}/${componentName}${componentExt}`
+  const indexPath = `${componentDir}/index${indexExt}`
 
   // Our index template is super straightforward, so we'll just inline it for now.
-  const indexTemplate = `\
+  const indexTemplate = prettify(`\
   export * from './${componentName}';
   export { default } from './${componentName}';
-  `
+  `)
+
   // Check if componentName is provided
   if (!componentName) {
     console.error('A component name must be given.')
@@ -39,11 +72,12 @@ async function main() {
   }
 
   // Check to see if a directory at the given path exists
-  const fullPathToParentDir = path.resolve('src/components')
-  console.log(fullPathToParentDir)
+  const fullPathToParentDir = path.resolve(componentDir)
   if (!fs.existsSync(fullPathToParentDir)) {
     console.error(
-      'Sorry, you need to create a parent directory.\n(new-component is looking for a directory at ./src/components)'
+      `Sorry, you need to create a parent directory.\n(new-component is looking for a directory at ${
+        program.opts().dir
+      })`
     )
     process.exit(0)
   }
@@ -63,9 +97,10 @@ async function main() {
     'utf-8'
   )
   const modifiedTemplate = template.replace(/COMPONENT_NAME/, componentName)
-  await fsp.writeFile(componentPath, modifiedTemplate, 'utf-8')
 
   // Format it using prettier, to ensure style consistency, and write to file.
+  const prettyTemplate = prettify(modifiedTemplate)
+  await fsp.writeFile(componentPath, prettyTemplate, 'utf-8')
 
   // We also need the `index.js` file, which allows easy importing.
   await fsp.writeFile(indexPath, indexTemplate, 'utf-8')
